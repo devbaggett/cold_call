9d382c3ea94ae7f5cebc1d5efba65a2d9056a341
devbaggett <devabaggett@gmail.com>
2018-05-07 14:05:35 -0700

    added random number generator with colors

--- Cold Call/ViewController.swift
+++ Cold Call/ViewController.swift
@@ -10,17 +10,28 @@ import UIKit
 
 class ViewController: UIViewController {
     @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var numberLabel: UILabel!
+    
     let names = [ "Annie", "Pat", "Kostya", "John", "JP"]
     
-
     @IBAction func coldcallButton(_ sender: UIButton) {
         nameLabel.text = names[Int(arc4random_uniform(UInt32(names.count)))]
+        numberLabel.text = String(arc4random_uniform(5) + 1)
+        if (numberLabel.text == "1" || numberLabel.text == "2") {
+            numberLabel.textColor = UIColor.red
+        }
+        else if (numberLabel.text == "3" || numberLabel.text == "4") {
+            numberLabel.textColor = UIColor.orange
+        }
+        else if (numberLabel.text == "5") {
+            numberLabel.textColor = UIColor.green
+        }
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         nameLabel.text = "Ready?"
-        // Do any additional setup after loading the view, typically from a nib.
+        numberLabel.text = ""
     }
 
     override func didReceiveMemoryWarning() {
@@ -30,4 +41,3 @@ class ViewController: UIViewController {
 
 
 }
-
